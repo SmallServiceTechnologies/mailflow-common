0da282c9955a0084fd153dbb3a4c7b3f5b8c40eb
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-12 15:48:55 +0200

    Spotless

--- src/main/java/de/flowsuite/mailflow/common/dto/LlmServiceRequest.java
+++ src/main/java/de/flowsuite/mailflow/common/dto/LlmServiceRequest.java
@@ -2,9 +2,10 @@ package de.flowsuite.mailflow.common.dto;
 
 import de.flowsuite.mailflow.common.entity.MessageCategory;
 import de.flowsuite.mailflow.common.entity.User;
+
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
-public record LlmServiceRequest (User user, String text, List<MessageCategory> categories) {}
+public record LlmServiceRequest(User user, String text, List<MessageCategory> categories) {}
