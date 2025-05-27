7b0afe2cf4ae6e199d526b2bccd26e57c4e3c561
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-27 22:49:25 +0200

    Small improvements

--- src/main/java/de/flowsuite/mailflow/common/dto/RagServiceRequest.java
+++ src/main/java/de/flowsuite/mailflow/common/dto/RagServiceRequest.java
@@ -1,9 +1,8 @@
 package de.flowsuite.mailflow.common.dto;
 
-import de.flowsuite.mailflow.common.entity.Customer;
-import de.flowsuite.mailflow.common.entity.User;
-
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
-public record RagServiceRequest(User user, Customer customer, String messageThread) {}
+public record RagServiceRequest(long userId, long customerId, List<ThreadMessage> messageThread) {}
