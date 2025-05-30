4102a6308d5fd8186119ac077620ea64b25b4454
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-30 14:13:25 +0200

    Getters and Setters

--- src/main/java/de/flowsuite/mailflow/common/dto/CategorisationResponse.java
+++ src/main/java/de/flowsuite/mailflow/common/dto/CategorisationResponse.java
@@ -3,11 +3,29 @@ package de.flowsuite.mailflow.common.dto;
 import de.flowsuite.mailflow.common.entity.MessageCategory;
 
 import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
-public record CategorisationResponse(
-        MessageCategory messageCategory,
-        String llmUsed,
-        Integer inputTokens,
-        Integer outputTokens,
-        Integer totalTokens) {}
+public class CategorisationResponse {
+
+    private MessageCategory messageCategory;
+    private String llmUsed;
+    private Integer inputTokens;
+    private Integer outputTokens;
+    private Integer totalTokens;
+
+    public CategorisationResponse(MessageCategory messageCategory, String llmUsed, Integer inputTokens, Integer outputTokens, Integer totalTokens) {
+        this.messageCategory = messageCategory;
+        this.llmUsed = llmUsed;
+        this.inputTokens = inputTokens;
+        this.outputTokens = outputTokens;
+        this.totalTokens = totalTokens;
+    }
+
+
+
+}
