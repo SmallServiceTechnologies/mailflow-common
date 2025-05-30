95d9f7e82ab103c4859635f651692714be0ac354
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-30 02:59:57 +0200

    Spotless

--- src/main/java/de/flowsuite/mailflow/common/dto/UpdateCustomerCrawlStatusRequest.java
+++ src/main/java/de/flowsuite/mailflow/common/dto/UpdateCustomerCrawlStatusRequest.java
@@ -4,4 +4,5 @@ import jakarta.validation.constraints.NotNull;
 
 import java.time.ZonedDateTime;
 
-public record UpdateCustomerCrawlStatusRequest(@NotNull Long id, @NotNull ZonedDateTime lastCrawlAt, @NotNull ZonedDateTime nextCrawlAt) {}
+public record UpdateCustomerCrawlStatusRequest(
+        @NotNull Long id, @NotNull ZonedDateTime lastCrawlAt, @NotNull ZonedDateTime nextCrawlAt) {}
